0231b4ae2538352be433b0a494f5a404f4fb1d42
SlinkKnight <83033666+SlinkKnight@users.noreply.github.com>
2022-06-24 22:40:22 -0300

    Update test.py

--- test.py
+++ test.py
@@ -6,4 +6,4 @@ imagem=r.json()['message']
 payload={
     'content': imagem
 }
-r=requests.post("https://discord.com/api/webhooks/976193399810064424/-2viIUqPyvMaLTHJcHwDNQ9F4EfkVLJbahs7F6EUHU41T-MkI6hz2JZcfyzWy05qCWgN",data=payload)
+r=requests.post(open("hooks.txt", 'r'),data=payload)
